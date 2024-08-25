7705cdffe115e877f5f3f9227219e340200e4771
Arun Kumar <arunkakorp@gmail.com>
2024-08-25 19:33:38 +0530

    Added custom action to FormAction for the
    djangocms_form_builder plugin. Custom action doesn't do
    anything at the moment except log the values to the log file.

--- shop/apps/main/apps.py
+++ shop/apps/main/apps.py
@@ -1,14 +1,29 @@
-from django.apps import AppConfig
 from django.urls import path
 from oscar import config
+import logging
+from django.utils.translation import gettext_lazy as _
 
-#class MainConfig(AppConfig):
-#    default_auto_field = 'django.db.models.BigAutoField'
-#    name = 'shop.apps.main'
+logger = logging.getLogger("shop.apps.main")
 
 class MainShop(config.Shop):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop.apps.main'
+
+    def ready(self):
+        super().ready()
+
+        from djangocms_form_builder import actions
+        @actions.register
+        class AddToMailingList(actions.FormAction):
+            verbose_name = _("Add to mailing list")
+
+            def execute(self, form, request):
+                logger.warn("Called from Add to mailing list FormAction")
+                logger.warn(form)
+                logger.debug(request)
+
+        logger.warn("Inside FormAction execute")
+        logger.debug(actions._action_registry)
 
     def get_urls(self):
         urls = super().get_urls()
