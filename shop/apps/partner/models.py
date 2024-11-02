fd8734a43e04c2e8eb202adc84cb913b5ed9769e
Arun Kumar <arunkakorp@gmail.com>
2024-11-03 01:07:42 +0530

    Added seller field to Partner model.
    Currently a Partner belongs to one Seller, we might
    move to a situation in the future where the relationship between Seller
    and Partner is many to many. One Seller can have multiple fulfilment
    Partners and a fulfilment Partner can work with multiple sellers.
    
    We will change the data model accordingly in the future

--- shop/apps/partner/models.py
+++ shop/apps/partner/models.py
@@ -1,1 +1,29 @@
-from oscar.apps.partner.models import * 
+from oscar.apps.partner.abstract_models import *
+from oscar.apps.address.abstract_models import AbstractPartnerAddress
+""" from oscar.apps.partner.models import (
+    PartnerAddress,
+    StockRecord,
+    StockAlert
+) """
+from shop.apps.seller.models import Seller
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from oscar.core.loading import is_model_registered
+
+
+class Partner(AbstractPartner):
+    seller = models.ForeignKey(Seller, verbose_name=_("Seller"), blank=False, null=False, default=settings.ZITE69_MAIN_SELLER_ID, on_delete=models.CASCADE)
+
+if not is_model_registered('partner', 'PartnerAddress'):
+    class PartnerAddress(AbstractPartnerAddress):
+        pass
+
+if not is_model_registered('partner', 'StockRecord'):
+    class StockRecord(AbstractStockRecord):
+        pass
+
+if not is_model_registered('partner', 'StockAlert'):
+    class StockAlert(AbstractStockAlert):
+        pass
+
+
