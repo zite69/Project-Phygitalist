cf38e7e90830804bee24c27e8a7ba83abe0f6558
Arun Kumar <arunkakorp@gmail.com>
2024-11-03 01:09:49 +0530

    Removed code for resolving CMS urls that never
    quite worked. Tried to integrate both CMS and django-oscar to work
    on the root path of the site - ie. on the home page.
    
    Still need to figure out how to achieve this

--- shop/urls.py
+++ shop/urls.py
@@ -26,42 +26,20 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.http import Http404, HttpResponseRedirect
 
 
-class OscarCMSResolver(object):
-    def resolve(self, request):
-        path = request.path
-        oscar_urls = apps.get_app_config('main').urls[0]
-        for url_pattern in oscar_urls:
-            try:
-                resolver_match = resolve(path)
-                if resolver_match.url_name is not None and resolver_match.url_name in url_pattern.name:
-                    args = [arg for arg in resolver_match.args if arg is not None]
-                    kwargs = {key: value for key, value in resolver_match.kwargs.items() if value is not None}
-                    return resolver_match.func(request, *args, **kwargs)
-            except Http404:
-                pass
-        cms_resolver_match = resolve(path, urlconf='cms.urls')
-        args = [arg for arg in cms_resolver_match.args if arg is not None]
-        kwargs = {key: value for key, value in cms_resolver_match.kwargs.items() if value is not None}
-        return cms_resolver_match.func(request, *args, **kwargs)
-        #return resolve(path, urlconf='cms.urls').func(request)
-
-def oscar_cms_resolver(request):
-    return OscarCMSResolver().resolve(request)
-
 urlpatterns = i18n_patterns(
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('admin/', admin.site.urls),
     path('filer/', include('filer.urls')),
     path('accounts/', include('allauth.urls')),
+    path('messages/', include('django_messages.urls')),
     #path('', home, name='home'),
     path('shop/', include(apps.get_app_config('main').urls[0])),
     path('otp/', include((apps.get_app_config('otp').urls[0], 'otp'), namespace='otp')),
+    path('membership/', include((apps.get_app_config('membership').urls[0], 'membership'), namespace='membership')),
+    #path('referrals/', include('pinax.referrals.urls', namespace='pinax_referrals')),
     path('', include('cms.urls')),
-    #path('', oscar_cms_resolver),
 )
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += debug_toolbar_urls()
-
+    urlpatterns = debug_toolbar_urls() + urlpatterns
