cf68d28c265fdd4d1008e9a2bf552613256bbd41
Arun Kumar <arunkakorp@gmail.com>
2024-11-27 21:49:56 +0530

    WIP commit. Just the updated templates for
    sending invites to sellers and CEOs to join us. This commit
    is for Edwin to continue to work on the page and form, while I get the
    backend code ready to handle the functionality

--- shop/apps/invitation/forms.py
+++ shop/apps/invitation/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.utils.translation import gettext as _
-
-#from account.models import EmailAddress
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from moneyed.classes import CLE
+from shop.apps.registration.models import SellerRegistration
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.validators import ValidationError as PhoneValidationError
 from .models import InvitationCode, Invitation
 
+User = get_user_model()
 
 class InviteForm(forms.Form):
     email_address = forms.EmailField()
@@ -28,3 +34,36 @@ class InvitationForm(forms.ModelForm):
         fields = [
             'email', 'phone', 'invite_type'
         ]
+        # widgets = {
+        #     "phone": PhoneNumberField(region='IN')
+        # }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit(name="submit", value="Send Invite"))
+
+    def clean_email(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        if email == "":
+            return cleaned_data
+
+        if User.objects.filter(email=email).exists():
+            raise ValidationError(_("A user with that email address already exists"))
+
+        if SellerRegistration.objects.filter(email=email).exists():
+            raise ValidationError(_("A user with that email address already exists"))
+
+        return cleaned_data
+
+    def clean_phone(self):
+        cleaned_data = super().clean()
+        phone = cleaned_data.get('phone')
+
+        if phone == "":
+            return cleaned_data
+
+        return cleaned_data
+
+    def clean(self):
+        return super().clean()
