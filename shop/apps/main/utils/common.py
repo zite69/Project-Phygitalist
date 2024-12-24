6eca90055aae56d3e53c86f0c0b4a8de183ab7ff
Arun Kumar <arunkakorp@gmail.com>
2024-12-24 17:17:39 +0530

    defined a function called get_deep_obj which given
    an object, gets its deeply nested child property.
    For instance if you have a request object and want to get
    request.user.seller_registration.phone then you could call it like
    >>> phone = get_deep_obj(request, 'user.seller_registration.phone')
    This is useful in the context of views when you want generic things to
    be done to the objects passed in to class methods

--- shop/apps/main/utils/common.py
+++ shop/apps/main/utils/common.py
@@ -1,5 +1,19 @@
 import warnings
 
+def get_deep_obj(obj, property_path):
+    """
+        Given an object, gets a property nested deep within it.
+        For example if you have a user with a profile and the profile has an address. 
+        You can use:
+        >>> address = get_deep_obj(user, 'profile.address')
+    """
+    head, *tail = property_path.split(".")
+    newobj = getattr(obj, head)
+    if not newobj:
+        return obj
+    else:
+        return get_deep_obj(newobj, ".".join(tail))
+
 def dump(obj, logger):
   for name in dir(obj):
     e = False
