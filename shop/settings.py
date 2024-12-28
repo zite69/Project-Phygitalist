63d3908b7145a2151f592a7656e803edae208283
Arun Kumar <arunkakorp@gmail.com>
2024-12-28 23:27:32 +0530

    Changed SmartDownloadMiddleware to ProxiedMiddleware
    for django-downloadview.

--- shop/settings.py
+++ shop/settings.py
@@ -213,7 +213,7 @@ else:
     #Enable caching only for non-DEBUG environments - Testing and Production
     MIDDLEWARE = ['django.middleware.cache.UpdateCacheMiddleware',] + MIDDLEWARE
     MIDDLEWARE += ['django.middleware.cache.FetchFromCacheMiddleware', 
-                   'django_downloadview.SmartDownloadMiddleware',
+                   'django_downloadview.ProxiedMiddleware',
                   ]
 
 INTERNAL_IPS = [
