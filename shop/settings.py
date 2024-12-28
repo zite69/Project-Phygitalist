9b3ef10f5b4cd800c025513072daecf008051099
Arun Kumar <arunkakorp@gmail.com>
2024-12-28 23:30:34 +0530

    Mis-spelled ProxiedDownloadMiddleware in MIDDLEWARES
    Have corrected the spelling

--- shop/settings.py
+++ shop/settings.py
@@ -213,7 +213,7 @@ else:
     #Enable caching only for non-DEBUG environments - Testing and Production
     MIDDLEWARE = ['django.middleware.cache.UpdateCacheMiddleware',] + MIDDLEWARE
     MIDDLEWARE += ['django.middleware.cache.FetchFromCacheMiddleware', 
-                   'django_downloadview.ProxiedMiddleware',
+                   'django_downloadview.ProxiedDownloadMiddleware',
                   ]
 
 INTERNAL_IPS = [
