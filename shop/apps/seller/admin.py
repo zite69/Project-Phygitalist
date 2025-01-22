e5f868dffc4500bcd0681efde6d9be96878d7378
Arun Kumar <arunkakorp@gmail.com>
2025-01-22 10:17:59 +0530

    Update seller user to staff if they are approved
    or remove staff access if they are rejected. Oscar checks
    for is_staff permission before giving access to the dashboard

--- shop/apps/seller/admin.py
+++ shop/apps/seller/admin.py
@@ -11,6 +11,8 @@ import logging
 logger = logging.getLogger(__package__)
 
 class SellerAdminForm(forms.ModelForm):
+    action = forms.CharField(widget=forms.HiddenInput())
+
     class Meta:
         model = Seller
         widgets = {
@@ -22,7 +24,7 @@ class SellerAdminForm(forms.ModelForm):
         fields = ['name', 'handle', 'user', 'admin', 'ceo', 
                   'gstin', 'gstin_file', 'gstin_file_verified',
                   'pan', 'pan_file', 'pan_file_verified',
-                  'signature_file', 'signature_file_verified', 'approval_notes', 'approved']
+                  'signature_file', 'signature_file_verified', 'approval_notes', 'approved', 'action']
 
     def hide_group(self, group):
         for fld in ['', '_file', '_file_verified']:
@@ -51,12 +53,39 @@ class SellerAdmin(admin.ModelAdmin):
     list_display = ('handle', 'user', 'approved')
     search_fields = ('handle', 'user__username', 'user__email', 'user__phone')
 
-    @csrf_protect_m
-    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
-        if request.method == 'POST' and '_approve' in request.POST:
-            logger.debug("Called from custom button")
+    def response_change(self, request, obj):
+        if 'action' in request.POST:
+            action = request.POST['action']
+            logger.debug(f"Got Seller ModelAdmin custom action: {action}")
+            if action == 'approved':
+                if not obj.user.is_staff:
+                    obj.user.is_staff = True
+                    obj.user.save()
+            elif action == 'rejected':
+                if obj.user.is_staff:
+                    obj.user.is_staff = False
+                    obj.user.save()
+        else:
+            logger.debug("Did not get any action")
 
-        return super().changeform_view(request, object_id, form_url, extra_context)
+        return super().response_change(request, obj)
+
+    # @csrf_protect_m
+    # def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
+    #     logger.debug("Inside changeform_view")
+    #     logger.debug(request.POST)
+    #     action = request.POST.get('action', '')
+    #     if action == 'approved':
+    #         logger.debug("We got approved")
+    #     elif action == 'rejected':
+    #         logger.debug("We got rejected")
+    #     else:
+    #         logger.debug(f"We got unknown action: {action}")
+
+    #     if request.method == 'POST' and '_approve' in request.POST:
+    #         logger.debug("Called from custom button")
+
+    #     return super().changeform_view(request, object_id, form_url, extra_context)
 
     def render_change_form(self, request, context, add=False, change=False, form_url="", obj=None):
         if obj:
