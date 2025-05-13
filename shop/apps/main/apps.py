ab248b8bf7ecf049cd88a656036e0228e00cdbb5
arun <arunkakorp@gmail.com>
2025-05-14 02:34:44 +0530

    Removed old commented out code. Also dont remove
    the dashboard from the oscar URLs. Till we setup the
    proper Apphook

--- shop/apps/main/apps.py
+++ shop/apps/main/apps.py
@@ -20,37 +20,14 @@ class MainShop(config.Shop):
         self.test_view = test
         self.fivehundred = fivehundred
 
-        # from djangocms_form_builder import actions
-        # from djangocms_form_builder.models import FormEntry
-
-        # from .utils.email import send_waitlist_welcome
-
-        # @actions.register
-        # class AddToMailingList(actions.FormAction):
-        #     verbose_name = _("Add to mailing list")
-
-        #     def execute(self, form, request):
-        #         email = form.cleaned_data['email']
-        #         logger.debug(f"In AddToMailingList execute got email: {email}")
-        #         getonwaitlist = FormEntry.objects.filter(form_name__iexact='getonwaitlist').filter(entry_data__email=email).order_by('entry_created_at')
-
-        #         if getonwaitlist.count() > 1:
-        #             logger.debug("User already in the waitlist")
-        #             ids = getonwaitlist.values_list("id", flat=True)
-        #             logger.debug(f"Deleting duplicate entries with ids: {ids}")
-        #             getonwaitlist.exclude(pk__in=list(ids[:1])).delete()
-        #             messages.error(request, "You have already added your name to the waitlist. Please wait for your confirmation email")
-        #         else:
-        #             count = send_waitlist_welcome(form.cleaned_data['email'])
-        #             logger.debug(f"Got response back from send email call: {count}")
-        #             messages.success(request, "You have been added to the waiting list. Please expect an email from us soon!")
-
     def get_urls(self):
         urls = super().get_urls()
         urls.pop(0) #Remove the RedirectView that redirects the home page, Our CMS will handle this view
         urls.pop(0) #Remove the url spelled /catalogue/ and replace with /catalog/ below
+        # urls.pop(4) #Remove the dashboard urls because the CMS Apphook will handle this
         urls.insert(0, path('catalog/', self.catalogue_app.urls))
         # urls.insert(0, path('test/', self.test_view))
         # urls.insert(0, path('error/', self.fivehundred))
 
         return urls
+
