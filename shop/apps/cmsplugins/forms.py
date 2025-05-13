50121920fe1c20fb40f391f52a21bce32dfb306e
arun <arunkakorp@gmail.com>
2025-05-14 02:40:09 +0530

    Commented out the CMSPlugin Forms.

--- shop/apps/cmsplugins/forms.py
+++ shop/apps/cmsplugins/forms.py
@@ -5,23 +5,23 @@ from shop.apps.catalogue.models import Product
 from django.utils.translation import gettext as _
 import select2.fields
 
-class ProductWidget(s2forms.ModelSelect2Widget):
-    search_fields = [
-        "title__icontains",
-        "upc__icontains",
-    ]
+# class ProductWidget(s2forms.ModelSelect2Widget):
+#     search_fields = [
+#         "title__icontains",
+#         "upc__icontains",
+#     ]
 
-class FeaturedProductForm(forms.ModelForm):
-    product = select2.fields.ChoiceField(
-            choices=Product.objects_choices.all(),
-            overlay="Select a Product"
-            )
-    # product = s2forms.ModelSelect2Field(
-    #         queryset=Product.objects.all(),
-    #         widget=s2forms.Select2Widget(attrs={"data-placeholder": "Select a Product"}),
-    #         label=_("Product")
-    #         )
+# class FeaturedProductForm(forms.ModelForm):
+#     product = select2.fields.ChoiceField(
+#             choices=Product.objects_choices.all(),
+#             overlay="Select a Product"
+#             )
+#     # product = s2forms.ModelSelect2Field(
+#     #         queryset=Product.objects.all(),
+#     #         widget=s2forms.Select2Widget(attrs={"data-placeholder": "Select a Product"}),
+#     #         label=_("Product")
+#     #         )
 
-    class Meta:
-        model = models.FeaturedProduct
-        fields = '__all__'
+#     class Meta:
+#         model = models.FeaturedProduct
+#         fields = '__all__'
