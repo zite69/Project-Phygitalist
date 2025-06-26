19492ae1600d613eb4856a625e3b6ee87a39a8cb
Arun Kumar <arunkakorp@gmail.com>
2025-06-26 11:57:17 +0530

    Added Import/Export functionality
    to particular models of Oscar

--- shop/apps/catalogue/admin.py
+++ shop/apps/catalogue/admin.py
@@ -1,7 +1,7 @@
 from oscar.apps.catalogue.admin import *  
 from django.contrib import admin
-from shop.apps.catalogue.models import Product, Category, ProductAttribute
-from import_export.admin import ImportExportModelAdmin
+from shop.apps.catalogue.models import Product, Category, ProductAttribute, ProductClass
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 from import_export import resources
 
 class ProductResource(resources.ModelResource):
@@ -16,19 +16,27 @@ class ProductAttributeResource(resources.ModelResource):
     class Meta:
         model = ProductAttribute
 
+class ProductClassResource(resources.ModelResource):
+    class Meta:
+        model = ProductClass
+
 admin.site.unregister(ProductAttribute)
 admin.site.unregister(Product)
 admin.site.unregister(Category)
+admin.site.unregister(ProductClass)
 
 @admin.register(Product)
-class ProductAdmin(ImportExportModelAdmin):
+class ProductImportExportModelAdmin(ImportExportModelAdmin):
     resource_classes = [ProductResource]
 
 @admin.register(Category)
-class CategoryAdmin(ImportExportModelAdmin):
+class CategoryImportExportModelAdmin(ImportExportModelAdmin):
     resource_classes = [CategoryResource]
 
 @admin.register(ProductAttribute)
-class ProductAttributeAdmin(ImportExportModelAdmin):
+class ProductAttributeImportExportModelAdmin(ImportExportModelAdmin):
     resource_classes = [ProductAttributeResource]
 
+@admin.register(ProductClass)
+class ProductClassImportExportModelAdmin(ImportExportModelAdmin):
+    resource_classes = [ProductClassResource]
