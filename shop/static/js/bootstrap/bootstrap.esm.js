8a0928ffbe84130b2f89069b24a3da3557a847d0
Arun Kumar <arunkakorp@gmail.com>
2025-08-16 13:29:10 +0530

    stripped all source maps because it was causing
    errors with ManifestStaticFileStorage system

--- shop/static/js/bootstrap/bootstrap.esm.js
+++ shop/static/js/bootstrap/bootstrap.esm.js
@@ -4444,4 +4444,3 @@ enableDismissTrigger(Toast);
 defineJQueryPlugin(Toast);
 
 export { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip };
-//# sourceMappingURL=bootstrap.esm.js.map
